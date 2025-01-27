3ee8a45a01276bbfa704a7ab0a11da848c2f32a8
Alessandro Cardenas-Engle <ci6117or@go.minnstate.edu>
2025-01-27 20:21:01 +0000

    Initial Commit

---
+++ Module2Challange/Program.cs
@@ -0,0 +1,17 @@
+﻿// Module2Challenge
+// Alessandro Cardenas-Engle
+// Date: 1/27/25
+
+// Asks the user to enter number of miles driven
+Console.Write("Enter number of miles driven: ");
+double milesDriven = Convert.ToDouble(Console.ReadLine());
+
+// Asks the user to enter the gallons of gas used 
+Console.Write("How many gallons of gas did you use: ");
+double gasUsed = Convert.ToDouble(Console.ReadLine());
+
+// Calculates the miled per gallon
+double mpg = milesDriven / gasUsed;
+
+// Displays the mpg 
+Console.Write("Your miles per gallon for your trip is: " + mpg);
